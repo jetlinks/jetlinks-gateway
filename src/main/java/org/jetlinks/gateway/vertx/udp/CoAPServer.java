cd593293b6be20d2f3f41a0483b153dbd929c693
zhouhao <zh.sqy@qq.com>
2019-03-29 18:30:29 +0800

    优化

--- src/main/java/org/jetlinks/gateway/vertx/udp/CoAPServer.java
+++ src/main/java/org/jetlinks/gateway/vertx/udp/CoAPServer.java
@@ -7,11 +7,11 @@ import io.vertx.core.net.SocketAddress;
 import lombok.extern.slf4j.Slf4j;
 import org.jetlinks.coap.CoapPacket;
 import org.jetlinks.coap.exception.CoapException;
+import org.jetlinks.protocol.message.DeviceMessage;
 import org.jetlinks.protocol.message.codec.CoAPMessage;
 import org.jetlinks.protocol.message.codec.EncodedMessage;
 import org.jetlinks.protocol.message.codec.FromDeviceMessageContext;
 import org.jetlinks.protocol.message.codec.Transport;
-import org.jetlinks.protocol.metadata.DeviceMetadata;
 import org.jetlinks.protocol.device.DeviceInfo;
 import org.jetlinks.protocol.device.DeviceOperation;
 
@@ -36,7 +36,7 @@ public abstract class CoAPServer extends UDPServer {
         }
     }
 
-    protected abstract DeviceOperation getDevice(SocketAddress address, CoapPacket packet);
+    protected abstract UDPDeviceSession getDevice(SocketAddress address, CoapPacket packet);
 
     protected void unregisterClient(String idOrDeviceId) {
         sessionManager.unregister(idOrDeviceId);
@@ -44,12 +44,13 @@ public abstract class CoAPServer extends UDPServer {
 
     protected void handleCoAPMessage(SocketAddress address, CoapPacket packet) {
         log.info("接受到CoAP消息:{}", address.host(), address.port(), packet.toString(true, false, true, false));
-        DeviceOperation deviceOperation = getDevice(address, packet);
-        if (deviceOperation != null) {
+        UDPDeviceSession session = getDevice(address, packet);
+        if (session != null) {
+            DeviceOperation deviceOperation = session.getOperation();
             DeviceInfo deviceInfo = deviceOperation.getDeviceInfo();
             String protocol = deviceInfo.getProtocol();
             CoAPMessage coapMessage = new CoAPMessage(deviceInfo.getId(), packet);
-            protocolSupports.getProtocol(protocol)
+            DeviceMessage message = protocolSupports.getProtocol(protocol)
                     .getMessageCodec()
                     .decode(Transport.CoAP, new FromDeviceMessageContext() {
 
@@ -60,13 +61,7 @@ public abstract class CoAPServer extends UDPServer {
 
                         @Override
                         public void sendToDevice(EncodedMessage message) {
-                            socket.send(Buffer.buffer(message.getByteBuf()), address.port(), host, result -> {
-                                if (!result.succeeded()) {
-                                    log.error("发送CoAP消息失败:{}", message.toString(), result.cause());
-                                } else if (log.isDebugEnabled()) {
-                                    log.debug("发送CoAP消息成功:{}", message.toString());
-                                }
-                            });
+                            session.send(message);
                         }
 
                         @Override
@@ -80,6 +75,7 @@ public abstract class CoAPServer extends UDPServer {
                         }
 
                     });
+            messageConsumer.accept(session, message);
         }
     }
 
