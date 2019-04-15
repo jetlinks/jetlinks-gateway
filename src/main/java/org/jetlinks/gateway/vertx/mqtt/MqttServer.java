9aff89c51c941fe5a6e983d113f044c6e801ef17
zhouhao <zh.sqy@qq.com>
2019-04-15 14:19:16 +0800

    优化消息收发

--- src/main/java/org/jetlinks/gateway/vertx/mqtt/MqttServer.java
+++ src/main/java/org/jetlinks/gateway/vertx/mqtt/MqttServer.java
@@ -162,7 +162,7 @@ public class MqttServer extends AbstractVerticle {
                         String topicName = message.topicName();
                         Buffer buffer = message.payload();
                         if (logger.isDebugEnabled()) {
-                            logger.debug("收到设备[{}]消息{}=>{}", topicName, clientId, buffer.toString());
+                            logger.debug("收到设备[{}]消息[{}]=>{}", clientId, topicName, buffer.toString());
                         }
                         try {
                             //消息协议
